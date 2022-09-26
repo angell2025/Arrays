70c6e13813265df71553a67e1425a9aa18b39506
Angel Garcia <angel.garcia@mrjaffesclass.com>
2022-09-26 11:41:34 -0700

    Completed Array

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,5 +9,15 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String[] strArray = {"Alpha","Bravo","Charlie","Delta","Echo"};
+        for (int i = 0 ; i < strArray.length; i++)   {
+            System.out.println(strArray[i]);
+        }
+        for (String value : strArray)  {
+            System.out.print(value);
+        }
+        for (String value : strArray)  {
+            System.out.print(value + "-");
+        }
     }
 }
